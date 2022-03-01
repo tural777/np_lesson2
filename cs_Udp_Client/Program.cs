bfd629923352a75bf904dbbbc2b23a9174490b54
t.novruzov <t.novruzov@STHQ0118-01>
2022-03-01 20:17:27 +0400

    Add project files.

---
+++ cs_Udp_Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace cs_Udp_Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            var client = new Socket(
+               AddressFamily.InterNetwork,
+               SocketType.Dgram,
+               ProtocolType.Udp);
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            var port = 45678;
+            var remoteEP = new IPEndPoint(ip, port);
+
+            var msg = "";
+            byte[] buffer = null;
+
+            while (true)
+            {
+                msg = Console.ReadLine();
+                buffer = Encoding.Default.GetBytes(msg);
+                client.SendTo(buffer, remoteEP);
+            }
+
+        }
+    }
+}
---
+++ cs_Udp_Server/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_Udp_Server
+{
+    class Program
+    {
+
+        /*
+        static void Main()
+        {
+            var listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            var port = 45678;
+            var listenerEP = new IPEndPoint(ip, port);
+            listener.Bind(listenerEP);
+
+
+            var buffer = new byte[ushort.MaxValue - 30];
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+            var len = 0;
+            var msg = "";
+
+            while (true)
+            {
+                len = listener.ReceiveFrom(buffer, ref remoteEP);
+                msg = Encoding.Default.GetString(buffer, 0, len);
+                Console.WriteLine($"{remoteEP}: {msg}");
+            }
+        }
+        */
+
+
+
+        static void Main() => MainAsync().GetAwaiter().GetResult();
+
+        static async Task MainAsync()
+        {
+            var listener = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Dgram,
+                ProtocolType.Udp);
+
+            var ip = IPAddress.Parse("127.0.0.1");
+            var port = 45678;
+            var listenerEP = new IPEndPoint(ip, port);
+            listener.Bind(listenerEP);
+
+
+            EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+            var buffer = new byte[ushort.MaxValue - 30];
+            var segment = new ArraySegment<byte>(buffer);
+
+            while (true)
+            {
+                var res = await listener.ReceiveFromAsync(segment, SocketFlags.None, remoteEP);
+                var endPoint = res.RemoteEndPoint;
+                var len = res.ReceivedBytes;
+                var msg = Encoding.Default.GetString(buffer, 0, len);
+                Console.WriteLine($"{endPoint}: {msg}");
+            }
+        }
+    }
+}
