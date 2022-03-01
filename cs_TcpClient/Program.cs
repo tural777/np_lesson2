84e34ffe51b9d36b16c05f31308b66f434381383
Tural Novruzov <TuraliNovruzov@outlook.com>
2022-03-01 21:02:16 +0400

    add TcpListener & TcpClient

---
+++ cs_TcpClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace cs_TcpClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            var client = new TcpClient();
+            client.Connect("127.0.0.1", 27001);
+
+            var stream = client.GetStream();
+
+            var bw = new BinaryWriter(stream);
+            var br = new BinaryReader(stream);
+
+            while (true)
+            {
+                bw.Write(Console.ReadLine());
+                Console.WriteLine($"Answer: {br.ReadString()}");
+            }
+
+
+        }
+    }
+}
---
+++ cs_TcpListener/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace cs_TcpListener
+{
+    class Program
+    {
+        static void Main()
+        {
+            var listener = new TcpListener(IPAddress.Parse("10.1.18.4"), 45678);
+            listener.Start(10);
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                var stream = client.GetStream();
+
+                var bw = new BinaryWriter(stream);
+                var br = new BinaryReader(stream);
+
+                while (true)
+                {
+                    var data = br.ReadString();
+                    Console.WriteLine(data);
+                    bw.Write("Roger that!");
+                }
+            }
+
+        }
+    }
+}
